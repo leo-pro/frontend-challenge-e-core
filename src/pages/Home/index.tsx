2dc1d75c9d3c26e63f1520ba8f33473583b2951e
Leonardo Alves <leoroberto.alves@hotmail.com>
2022-04-18 17:31:07 -0300

    chore: install and configure eslint, prettier, husky and lint-staged

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,42 +1,42 @@
-import { Flex, SimpleGrid, Heading, VStack, Spinner } from "@chakra-ui/react";
-import { useEffect, useMemo, useState } from "react";
-import { CardTeam } from "../../components/Cards/Team";
-import { FilterInput } from "../../components/FilterInput";
-import useDebounce from "../../hooks/useDebounce";
-import { api } from "../../services/api";
+import {Flex, SimpleGrid, Heading, VStack, Spinner} from '@chakra-ui/react'
+import {useEffect, useMemo, useState} from 'react'
+import {CardTeam} from '../../components/Cards/Team'
+import {FilterInput} from '../../components/FilterInput'
+import useDebounce from '../../hooks/useDebounce'
+import {api} from '../../services/api'
 
 interface Team {
-  id: string;
-  name: string;
+  id: string
+  name: string
 }
 
 export function Home() {
-  const [filterInput, setFilterInput] = useState("");
-  const [teams, setTeams] = useState<Team[]>();
+  const [filterInput, setFilterInput] = useState('')
+  const [teams, setTeams] = useState<Team[]>()
 
-  const debouncedValue = useDebounce(filterInput, 500);
+  const debouncedValue = useDebounce(filterInput, 500)
 
   useEffect(() => {
     api
-      .get("/teams")
-      .then((response) => setTeams(response.data))
-      .catch((error) => console.log(error));
-  }, []);
+      .get('/teams')
+      .then(response => setTeams(response.data))
+      .catch(error => console.log(error))
+  }, [])
 
   const filteredTeams = useMemo(() => {
-    const searchLowerCase = debouncedValue.toLowerCase();
+    const searchLowerCase = debouncedValue.toLowerCase()
 
-    return teams?.filter((team) =>
-      team.name.toLowerCase().includes(searchLowerCase)
-    );
-  }, [debouncedValue, teams]);
+    return teams?.filter(team =>
+      team.name.toLowerCase().includes(searchLowerCase),
+    )
+  }, [debouncedValue, teams])
 
   return (
     <Flex
-      width={{ md: "700px", xl: "900px", "2xl": "1100px" }}
-      mx={"256px"}
+      width={{md: '700px', xl: '900px', '2xl': '1100px'}}
+      mx={'256px'}
       marginTop="64px"
-      direction={"column"}
+      direction={'column'}
     >
       <VStack w="100%" spacing="32px">
         <Heading as="h1" size="xl">
@@ -44,21 +44,21 @@ export function Home() {
         </Heading>
         <FilterInput
           value={filterInput}
-          onChange={(searchValue) => setFilterInput(searchValue)}
+          onChange={searchValue => setFilterInput(searchValue)}
         />
       </VStack>
 
       {filteredTeams ? (
         <SimpleGrid w="100%" columns={4} spacing={6} my={10}>
-          {filteredTeams?.map((team) => (
+          {filteredTeams?.map(team => (
             <CardTeam key={team.id} id={team.id} name={team.name} />
           ))}
         </SimpleGrid>
       ) : (
         <Flex w="100%" alignItems="center" justifyContent="center" my={20}>
-          <Spinner size="xl" thickness="5px" speed={"0.4s"} />
+          <Spinner size="xl" thickness="5px" speed={'0.4s'} />
         </Flex>
       )}
     </Flex>
-  );
+  )
 }
